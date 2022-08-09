df731f795dcc1882dcdf56641d9986da99efd1a1
Joao Machado <jtomaspm.99@gmail.com>
2022-08-09 20:07:28 +0100

    project structure

---
+++ backend/twpop/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for twpop project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twpop.settings')
+
+application = get_asgi_application()
